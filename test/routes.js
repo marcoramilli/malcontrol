e730fbe25d04a3f01e48f9230ab192b618bffe6c
Marco Ramilli <marco.ramilli@iprel.it>
2014-03-08 18:33:48 +0100

    .trav

--- test/routes.js
+++ test/routes.js
@@ -2,101 +2,98 @@
  * Simple testing, must be enpowered.
  *
  */
-var server = require('../server');
 var assert = require('assert');
 var request = require('supertest');  
 
-tests = function(){
-  describe('Routing', function() {
-    var url = "http://localhost";
+describe('Routing', function() {
+  var url = "http://localhost";
 
-    //before(function(done) {
-    //console.log("Preparing Tests ... ");
-    //// In our tests we use the test db
-    ////mongoose.connect(config.db.mongodb); 
-    ////done();
-    //});
+  //before(function(done) {
+  //console.log("Preparing Tests ... ");
+  //// In our tests we use the test db
+  ////mongoose.connect(config.db.mongodb); 
+  ////done();
+  //});
 
-    describe('GET TopCountriresPhishers', function() {
-      it('should return JSON withing the top phishers countries', function(done) {
+  describe('GET TopCountriresPhishers', function() {
+    it('should return JSON withing the top phishers countries', function(done) {
 
-        request(url)
-        .get('/api/topcountriesphishers')
-        .expect('Content-Type', /json/)
-        .expect(200) //Status code(200);
-      .end(function(err,res) {
-        if (err) {
-          throw err;
-        }
-        done();
-      });//end
-      });//it
-    });//describe
+      request(url)
+      .get('/api/topcountriesphishers')
+      .expect('Content-Type', /json/)
+      .expect(200) //Status code(200);
+    .end(function(err,res) {
+      if (err) {
+        throw err;
+      }
+      done();
+    });//end
+    });//it
+  });//describe
 
-    describe('GET TopCountriesmalware', function() {
-      it('should return JSON withing the top malware countries', function(done) {
+  describe('GET TopCountriesmalware', function() {
+    it('should return JSON withing the top malware countries', function(done) {
 
-        request(url)
-        .get('/api/topcountriesmalware')
-        .expect('Content-Type', /json/)
-        .expect(200) //Status code(200);
-      .end(function(err,res) {
-        if (err) {
-          throw err;
-        }
-        done();
-      });//end
-      });//it
-    });//describe
-
-
-    describe('GET TopCountriethreats', function() {
-      it('should return JSON withing the top threat countries', function(done) {
-
-        request(url)
-        .get('/api/topcountriesthreats')
-        .expect('Content-Type', /json/)
-        .expect(200) //Status code(200);
-      .end(function(err,res) {
-        if (err) {
-          throw err;
-        }
-        done();
-      });//end
-      });//it
-    });//describe
+      request(url)
+      .get('/api/topcountriesmalware')
+      .expect('Content-Type', /json/)
+      .expect(200) //Status code(200);
+    .end(function(err,res) {
+      if (err) {
+        throw err;
+      }
+      done();
+    });//end
+    });//it
+  });//describe
 
 
-    describe('GET Total malware', function() {
-      it('should return JSON withing the total amount of processed malware', function(done) {
+  describe('GET TopCountriethreats', function() {
+    it('should return JSON withing the top threat countries', function(done) {
 
-        request(url)
-        .get('/api/totalmalware')
-        .expect(200) //Status code(200);
-      .end(function(err,res) {
-        if (err) {
-          throw err;
-        }
-        done();
-      });//end
-      });//it
-    });//describe
-
-    describe('GET Total threats', function() {
-      it('should return JSON withing the total amount of processed threats', function(done) {
-
-        request(url)
-        .get('/api/totalthreats')
-        .expect(200) //Status code(200);
-      .end(function(err,res) {
-        if (err) {
-          throw err;
-        }
-        done();
-      });//end
-      });//it
-    });//describe
+      request(url)
+      .get('/api/topcountriesthreats')
+      .expect('Content-Type', /json/)
+      .expect(200) //Status code(200);
+    .end(function(err,res) {
+      if (err) {
+        throw err;
+      }
+      done();
+    });//end
+    });//it
+  });//describe
 
 
-  });//Routing Tests
-}//function test
+  describe('GET Total malware', function() {
+    it('should return JSON withing the total amount of processed malware', function(done) {
+
+      request(url)
+      .get('/api/totalmalware')
+      .expect(200) //Status code(200);
+    .end(function(err,res) {
+      if (err) {
+        throw err;
+      }
+      done();
+    });//end
+    });//it
+  });//describe
+
+  describe('GET Total threats', function() {
+    it('should return JSON withing the total amount of processed threats', function(done) {
+
+      request(url)
+      .get('/api/totalthreats')
+      .expect(200) //Status code(200);
+    .end(function(err,res) {
+      if (err) {
+        throw err;
+      }
+      done();
+    });//end
+    });//it
+  });//describe
+
+
+});//Routing Tests
