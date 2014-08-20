c82f1b286f58e1eefbc67c4bdcb8174eaec9f141
Marco Ramilli <mramilli@simplenetworks.it>
2014-08-20 07:49:46 +0200

    separation from serving web and scraping

---
+++ scrapers_service.js
@@ -0,0 +1,37 @@
+//General Imports
+var mongoose                 = require('mongoose');
+var _config                  = require('./conf/configs');
+var cluster                  = require('cluster');
+var application_root         = __dirname;
+var http                     = require('http');
+var path                     = require('path');
+var toobusy                  = require('toobusy'); //thank you guys, you saved me the day !
+//-----------------------------------------------------------------
+
+//Parser Imports
+var _urlquery_scraper        = require('./scrapers/queryurl_scraper');
+var _phishtank_scraper       = require('./scrapers/phishtank_scraper');
+var _webinspector_scraper    = require('./scrapers/webinspector_scraper');
+var _scumware_scraper        = require('./scrapers/scumware_scraper');
+var _malwr_scraper           = require('./scrapers/malwr_scraper');
+var _virusscan_scraper       = require('./scrapers/virusscan_scraper');
+var _commonGeoMalw           = require('./commons/save_malw');
+var _malware_domain_list     = require('./scrapers/malware_domain_list');
+var _malware_malc0de_scraper = require('./scrapers/malware_malc0de');
+var _malware_vxvault_scraper = require('./scrapers/malware_vxvault');
+//-----------------------------------------------------------------
+
+setInterval(function(){ if (!toobusy()) {_phishtank_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
+setInterval(function(){ if (!toobusy()) {_urlquery_scraper.goScraper();}}, _config.scrapers.urlquery_timer + parseInt(Math.random()*100000)); //_config.scrapers.urlquery_timer);
+setInterval(function(){ if (!toobusy()) {_webinspector_scraper.goScraper();}},_config.scrapers.webinspector_timer + parseInt(Math.random()*100000));//_config.scrapers.webinspector_timer);
+setInterval(function(){ if (!toobusy()) {_virusscan_scraper.goScraper();}},_config.scrapers.virusscanner_timer + parseInt(Math.random()*100000)); //_config.scrapers.virusscanner_timer);
+//TOFIX: fix the following scraper !
+setInterval(function(){ if (!toobusy()) {_malwr_scraper.goScraper();}}, _config.scrapers.malwr_timer + parseInt(Math.random()*100000)); //_config.scrapers.malwr_timer);
+setInterval(function(){ if (!toobusy()) {_malware_domain_list.goScraper();}}, _config.scrapers.malwr_timer + parseInt(Math.random()*100000)); //_config.scrapers.malwr_timer);
+setInterval(function(){ if (!toobusy()) {_malware_malc0de_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
+setInterval(function(){ if (!toobusy()) {_malware_vxvault_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
+
+process.on('uncaughtException', function globalErrorCatch(error, p){
+  console.error(error);
+  console.error(error.stack);
+});
--- server.js
+++ server.js
@@ -29,16 +29,16 @@ var toobusy                  = require('toobusy'); //thank you guys, you saved m
 //-----------------------------------------------------------------
 
 //Parser Imports
-var _urlquery_scraper        = require('./scrapers/queryurl_scraper');
-var _phishtank_scraper       = require('./scrapers/phishtank_scraper');
-var _webinspector_scraper    = require('./scrapers/webinspector_scraper');
-var _scumware_scraper        = require('./scrapers/scumware_scraper');
-var _malwr_scraper           = require('./scrapers/malwr_scraper');
-var _virusscan_scraper       = require('./scrapers/virusscan_scraper');
+//var _urlquery_scraper        = require('./scrapers/queryurl_scraper');
+//var _phishtank_scraper       = require('./scrapers/phishtank_scraper');
+//var _webinspector_scraper    = require('./scrapers/webinspector_scraper');
+//var _scumware_scraper        = require('./scrapers/scumware_scraper');
+//var _malwr_scraper           = require('./scrapers/malwr_scraper');
+//var _virusscan_scraper       = require('./scrapers/virusscan_scraper');
 var _commonGeoMalw           = require('./commons/save_malw');
-var _malware_domain_list     = require('./scrapers/malware_domain_list');
-var _malware_malc0de_scraper = require('./scrapers/malware_malc0de');
-var _malware_vxvault_scraper = require('./scrapers/malware_vxvault');
+//var _malware_domain_list     = require('./scrapers/malware_domain_list');
+//var _malware_malc0de_scraper = require('./scrapers/malware_malc0de');
+//var _malware_vxvault_scraper = require('./scrapers/malware_vxvault');
 //-----------------------------------------------------------------
 
 //DB Connections
@@ -66,20 +66,23 @@ if (cluster.isMaster){
       setInterval(function(){ if (!toobusy()) {_commonGeoMalw.geoLocMalwr();}}, _config.system.background_geoloc_service);
     } 
     else if (cluster.isWorker){
-      setInterval(function(){ if (!toobusy()) {_phishtank_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
-      setInterval(function(){ if (!toobusy()) {_urlquery_scraper.goScraper();}}, _config.scrapers.urlquery_timer + parseInt(Math.random()*100000)); //_config.scrapers.urlquery_timer);
-      setInterval(function(){ if (!toobusy()) {_webinspector_scraper.goScraper();}},_config.scrapers.webinspector_timer + parseInt(Math.random()*100000));//_config.scrapers.webinspector_timer);
-      setInterval(function(){ if (!toobusy()) {_virusscan_scraper.goScraper();}},_config.scrapers.virusscanner_timer + parseInt(Math.random()*100000)); //_config.scrapers.virusscanner_timer);
-      //TOFIX: fix the following scraper !
-      setInterval(function(){ if (!toobusy()) {_malwr_scraper.goScraper();}}, _config.scrapers.malwr_timer + parseInt(Math.random()*100000)); //_config.scrapers.malwr_timer);
-      setInterval(function(){ if (!toobusy()) {_malware_domain_list.goScraper();}}, _config.scrapers.malwr_timer + parseInt(Math.random()*100000)); //_config.scrapers.malwr_timer);
-      setInterval(function(){ if (!toobusy()) {_malware_malc0de_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
-      setInterval(function(){ if (!toobusy()) {_malware_vxvault_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
 
-      process.on('uncaughtException', function globalErrorCatch(error, p){
-        console.error(error);
-        console.error(error.stack);
-      });
+      //FOR STARTING THE SCRAPERS RUN: node scraper_service.js
+
+      //setInterval(function(){ if (!toobusy()) {_phishtank_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
+      //setInterval(function(){ if (!toobusy()) {_urlquery_scraper.goScraper();}}, _config.scrapers.urlquery_timer + parseInt(Math.random()*100000)); //_config.scrapers.urlquery_timer);
+      //setInterval(function(){ if (!toobusy()) {_webinspector_scraper.goScraper();}},_config.scrapers.webinspector_timer + parseInt(Math.random()*100000));//_config.scrapers.webinspector_timer);
+      //setInterval(function(){ if (!toobusy()) {_virusscan_scraper.goScraper();}},_config.scrapers.virusscanner_timer + parseInt(Math.random()*100000)); //_config.scrapers.virusscanner_timer);
+      ////TOFIX: fix the following scraper !
+      //setInterval(function(){ if (!toobusy()) {_malwr_scraper.goScraper();}}, _config.scrapers.malwr_timer + parseInt(Math.random()*100000)); //_config.scrapers.malwr_timer);
+      //setInterval(function(){ if (!toobusy()) {_malware_domain_list.goScraper();}}, _config.scrapers.malwr_timer + parseInt(Math.random()*100000)); //_config.scrapers.malwr_timer);
+      //setInterval(function(){ if (!toobusy()) {_malware_malc0de_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
+      //setInterval(function(){ if (!toobusy()) {_malware_vxvault_scraper.goScraper();}},_config.scrapers.phishtank_timer + parseInt(Math.random()*100000)); // _config.scrapers.phishtank_timer);
+
+      //process.on('uncaughtException', function globalErrorCatch(error, p){
+        //console.error(error);
+        //console.error(error.stack);
+      //});
 
       var express           = require('express');
       var basicRoutes       = require('./routes/basic');
